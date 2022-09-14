ac3c3aa13856a9fde999c4bbbcccd60364d4902a
VitorAlvesCarvalho <vitor.alves2402@gmail.com>
2022-09-14 17:26:03 -0300

    fix: ajustar estrutura de pastas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import { Home } from '@/modules/views';
+import { Home } from '@/modules/home/views';
 
 Vue.use(VueRouter);
 
