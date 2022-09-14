e1ced5bd1425ea1301a8a55f556871f9d22ca8f3
VitorAlvesCarvalho <vitor.alves2402@gmail.com>
2022-09-14 18:31:40 -0300

    feat: adicionar conteudo que é exibido na app bar

--- src/modules/home/components/AppBar/AppBar.vue
+++ src/modules/home/components/AppBar/AppBar.vue
@@ -1,20 +1,46 @@
 <template>
-  <header class="header">Header</header>
+  <header class="header">
+    <section class="header__day">
+      <p>{{ currentDay }}</p>
+    </section>
+    <section class="header__hours">
+      <p>{{ currentTime }}</p>
+    </section>
+  </header>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { dayFormatted } from '@/modules/home/helpers';
 
 @Component
-export default class AppBar extends Vue {}
+export default class AppBar extends Vue {
+  private date: Date = new Date();
+
+  public get currentDay() {
+    return dayFormatted(this.date.getUTCDay());
+  }
+
+  public get currentTime() {
+    return this.date.toLocaleTimeString();
+  }
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .header {
   background-color: #a910ff;
   text-align: center;
-  padding: 16px;
+  padding: 8px;
   color: white;
-  font-size: 24px;
+
+  &__day {
+    font-size: 20px;
+  }
+
+  &__hours {
+    margin-top: 4px;
+    font-size: 12px;
+  }
 }
 </style>
---
+++ src/modules/home/helpers/helpers.ts
@@ -0,0 +1,13 @@
+export const dayFormatted = (day: number) => {
+  const setDay = {
+    0: 'Domingo',
+    1: 'Segunda-feira',
+    2: 'Terça-feira',
+    3: 'Quarta-feira',
+    4: 'Quinta-feira',
+    5: 'Sexta-feira',
+    6: 'Sábado'
+  };
+
+  return setDay[day as keyof typeof setDay];
+};
